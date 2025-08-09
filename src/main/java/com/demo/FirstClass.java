847bb1eff925ca62ce917a53cb56863e7ccc2d36
Sharath583604 <sharathjogi04@gmail.com>
2025-08-09 18:07:41 +0530

    fifth commit

--- src/main/java/com/demo/FirstClass.java
+++ src/main/java/com/demo/FirstClass.java
@@ -5,7 +5,6 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("World");
-		System.out.println("hii namaste");
-		System.out.println("Pentagon");
+		
 	}
 }
